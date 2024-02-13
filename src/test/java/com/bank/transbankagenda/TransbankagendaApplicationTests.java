a8c061122dc8b92dc388f80baa524466be79b011
lr-alves <luizrodrigo7988@gmail.com>
2024-02-13 17:07:35 -0300

    initial commit

---
+++ src/test/java/com/bank/transbankagenda/TransbankagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.transbankagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransbankagendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
